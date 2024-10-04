d861bb20eec01e005142d5505f69e49b88ae29f4
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-10-04 14:24:37 +0530

    Get Current User With Saved ID

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -8,20 +8,29 @@ import 'package:http/http.dart' as http;
 class AuthProvider extends ChangeNotifier {
   bool? _isLoading = false;
   bool? _isUserLoggedIn = false;
-  String? _token;
+  int? _u_id;
+  String? _username;
 
   get isLoading => _isLoading;
   get isUserLoggedIn => _isUserLoggedIn;
-  get token=>_token;
+  get u_id=>_u_id;
+  get username=>_username;
 
-  void setUserLoggedIn(String token) async {
+  void userCheck()async{
+    SharedPreferences pref= await SharedPreferences.getInstance();
+    _u_id=pref.getInt("u_id")!;
+    getCurrentUser();
+    notifyListeners();
+  }
+
+  void setUserLoggedIn(int u_id) async {
     SharedPreferences pref = await SharedPreferences.getInstance();
 
     pref.setBool("isUserLoggedIn", true);
-    pref.setString("token", token);
+    pref.setInt("u_id", u_id);
 
     _isUserLoggedIn = pref.getBool("isUserLoggedIn")!;
-    _token = pref.getString("token")!;
+    _u_id = pref.getInt("u_id")!;
 
     notifyListeners();
   }
@@ -45,7 +54,7 @@ class AuthProvider extends ChangeNotifier {
         
       }else{
         final jsonData=jsonDecode(response.body);
-        setUserLoggedIn(jsonData['token']);
+        setUserLoggedIn(jsonData['u_id']);
       }
       print(response.statusCode);
       
@@ -56,6 +65,20 @@ class AuthProvider extends ChangeNotifier {
     return result;
   }
 
+
+  Future<void> getCurrentUser() async {
+    notifyListeners();
+    final response = await http.get(Uri.parse(API.baseUser+"/api/user/"+_u_id.toString()));
+    print(response.body);
+    if (response.statusCode == 200) {
+       final jsonData = json.decode(response.body);
+        _username=jsonData['u_fullname'];
+    } else {
+      throw Exception('Failed to load users');
+    }
+
+    notifyListeners();
   
+  }
 
 }
